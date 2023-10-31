15be7ea969d93280766fb362d03af4c8bcfa7893
Dev Johri <56020041+incruder1@users.noreply.github.com>
2023-10-31 07:35:29 +0530

    Update route.js

--- app/api/timeSeriesDaily/route.js
+++ app/api/timeSeriesDaily/route.js
@@ -7,7 +7,7 @@ export async function GET(req, res) {
   console.log("symbol", symbol, "apiKey", apiKey);
   try {
     const apiResponse = await fetch(
-      `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&outputsize=full&apikey=TK3TLI4UQ2XK3OBT`
+      `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&outputsize=full&apikey=1yco`
       //API Key is not used it sir because check nhi kr pa rha hu. baar baar iski limit over ho jaati hai. 
     );
     const data = await apiResponse.json();
