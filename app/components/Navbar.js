39c7110d666f39ce563d8ef6bcc69483694e0a61
Dev Johri <56020041+incruder1@users.noreply.github.com>
2023-10-31 17:24:06 +0530

    Update Navbar.js

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -9,19 +9,19 @@ import { Playfair_Display } from 'next/font/google'
 export default function PrimarySearchAppBar() {
   
   return (
-    <div className='flex-row'>
+    <div className='flex-row' style={{width:'100vw'}}>
       <nav className="bg-slate-500 shadow-lg">
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 ">
-          <div className="flex h-16 justify-start  ">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 flex h-16 justify-evenly ">
+          {/* <div className="flex h-16 justify-between  "> */}
             <Link href="/" className='flex flex-row '>
-              <div className="flex flex-shrink-0 items-center lg:text-3xl font-medium text-white
-               py-2 sm:text-xl">
+              <div className="flex   xs:hidden flex-shrink-2 items-center justify-center lg:text-3xl font-medium text-white
+               py-2 ">
                 GROWW STOCKS
               </div></Link> 
-              <div className=" ml-2 lg:ms-80 w-100 sm:w:50 sm:ml-5" >
+              <div className="lg:ms-80 w-100 sm:w:50 sm:ml-5" >
             <Search />           
               </div>
-          </div>
+          {/* </div> */}
         </div>
       </nav>
     </div>
