385d030ac090e6bcd14777ee4fbe77d2dd6960d0
Dev Johri <56020041+incruder1@users.noreply.github.com>
2023-10-31 07:51:39 +0530

    Updated route.js

--- app/api/tickerSearch/route.js
+++ app/api/tickerSearch/route.js
@@ -2,19 +2,19 @@
 
 // pages/api/tickerSearch.js
 
+ 
 export async function GET(req, res) {
   const { searchParams } = new URL(req.url);
     const keywords = searchParams.get("keywords") || "IBM";
-   try {
-      // const apiResponse = await fetch(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=tesco&apikey=demo`);
-      const apiResponse =await fetch(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${keywords}&apikey=1yco`)
-      // const apiData = await apiResponse.json();
+    const apiKey = process.env.API_KEY;
+    try {
+      const apiResponse = await fetch(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${keywords}&apikey=1yco`);
+      const apiData = await apiResponse.json();
       const name = apiData.bestMatches;
+      console.log(name)
       return new Response(JSON.stringify({ name: name }));
     } catch (error) {
       console.error(`Error: ${error.message}`);
-
       return new Response.json({ error: "Internal Server Error" });
     }
   };
-  
